b25cff09e32226151ea0a75c039fbf1cc1d5dd2d
Eric Solari <sliceofbytes@gmail.com>
2019-03-29 14:16:01 -0500

    Added firebase config to production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  version: '1.0.0-beta.1',
+  firebase: {
+    apiKey: 'AIzaSyDzw5cRoUJ6LnVPgrQzwRlSYwCbJt_Y_2I',
+    authDomain: 'skytale-io.firebaseapp.com',
+    databaseURL: 'https://skytale-io.firebaseio.com',
+    projectId: 'skytale-io',
+    storageBucket: 'skytale-io.appspot.com',
+    messagingSenderId: '197917449017'
+  }
 };
